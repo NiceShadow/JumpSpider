e0f2681a48e91fe1d6c3d0fc50efa2f968d3b53d
NiceShadow <39025636+NiceShadow@users.noreply.github.com>
2021-04-24 14:51:01 +0200

    Spider

---
+++ Itsybitsy Spideroo/Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        QualitySettings.vSyncCount = 1;
+        Application.targetFrameRate = 30;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
